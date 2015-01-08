d8bdf9c00c13f5e3e0b6937a4ad33d2ef2501157
jenkins <root@localhost>
2015-01-08 04:42:51 -0600

    Add evasive and awstats as submodule

--- extract-code.sh
+++ extract-code.sh
@@ -139,9 +139,9 @@ git submodule add https://github.com/AlbanAndrieu/ansible-cpp.git alban.andrieu.
 #git submodule deinit -f pagespeed
 git submodule add https://github.com/AlbanAndrieu/ansible-pagespeed.git alban.andrieu.pagespeed
 
-git submodule add https://github.com/AlbanAndrieu/ansible-evasive.git alban.andrieu.evasive
+git submodule add --force https://github.com/AlbanAndrieu/ansible-evasive.git alban.andrieu.evasive
 
-git submodule add https://github.com/AlbanAndrieu/ansible-awstats.git alban.andrieu.awstats
+git submodule add --force https://github.com/AlbanAndrieu/ansible-awstats.git alban.andrieu.awstats
 
 #git submodule deinit -f jmeter
 git submodule add https://github.com/AlbanAndrieu/ansible-jmeter.git alban.andrieu.jmeter
