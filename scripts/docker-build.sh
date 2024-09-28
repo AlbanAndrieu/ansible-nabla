ed6373eff11d30090d24aaf6960506f0b55fe8ac
AlbanAndrieu <alban.andrieu@free.fr>
2024-09-28 11:50:16 +0200

    Push nabla/ansible-nabla:1.0.4 same as ansible-jenkins-slave-docker

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,47 +5,49 @@ shopt -s extglob
 #set -ueo pipefail
 set -eo pipefail
 
-WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
-
 WORKING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export DOCKER_NAME=${DOCKER_NAME:-"ansible-nabla"}
-export DOCKER_TAG=${DOCKER_TAG:-"1.0.3"}
+export DOCKER_TAG=${DOCKER_TAG:-"1.0.4"}
 
 if [ -n "${DOCKER_BUILD_ARGS}" ]; then
   echo -e "${green} DOCKER_BUILD_ARGS is defined ${happy_smiley} : ${DOCKER_BUILD_ARGS} ${NC}"
 else
   echo -e "${red} ${double_arrow} Undefined build parameter ${head_skull} : DOCKER_BUILD_ARGS, use the default one ${NC}"
-  export DOCKER_BUILD_ARGS="--pull --build-arg ANSIBLE_VAULT_PASS=${ANSIBLE_VAULT_PASS} "
+  export DOCKER_BUILD_ARGS="--pull --network=host --add-host albandrieu.com:172.17.0.57 --build-arg ANSIBLE_VAULT_PASS=${ANSIBLE_VAULT_PASS} --build-arg CI_PIP_GITLABNABLA_TOKEN=${CI_PIP_GITLABNABLA_TOKEN}"
   #export DOCKER_BUILD_ARGS="--build-arg --no-cache"
   echo -e "${magenta} DOCKER_BUILD_ARGS : ${DOCKER_BUILD_ARGS} ${NC}"
 fi
 
-export DOCKER_FILE=${DOCKER_FILE:-"../docker/ubuntu20/Dockerfile"}
-export CST_CONFIG=${CST_CONFIG:-"docker/ubuntu20/config.yaml"}
+export DOCKER_FILE=${DOCKER_FILE:-"docker/ubuntu24/Dockerfile"}
+export CST_CONFIG=${CST_CONFIG:-"docker/ubuntu24/config.yaml"}
 
 # shellcheck source=/dev/null
 source "${WORKING_DIR}/docker-env.sh"
 
-echo -e "${green} Validating Docker ${NC}"
-echo -e "${magenta} hadolint ${WORKING_DIR}/${DOCKER_FILE} ${NC}"
-hadolint "${WORKING_DIR}/${DOCKER_FILE}" || true | tee -a docker-hadolint.log
-echo -e "${magenta} dockerfile_lint --json --verbose --dockerfile ${WORKING_DIR}/${DOCKER_FILE} ${NC}"
-dockerfile_lint --json --verbose --dockerfile "${WORKING_DIR}/${DOCKER_FILE}"|| true | tee -a docker-dockerfilelint.log
+"${WORKING_DIR}/docker-validate.sh"
 
 # shellcheck source=/dev/null
 source "${WORKING_DIR}/run-ansible.sh"
 
-WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+WORKING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-"${WORKING_DIR}/../clean.sh"
+# "${WORKING_DIR}/../clean.sh"
 
-echo -e "${green} Insalling roles version ${NC}"
-${ANSIBLE_GALAXY_CMD} install -r requirements.yml -p ./roles/ --ignore-errors
+# export DOCKER_BUILDKIT=1 # See https://github.com/moby/moby/issues/42261
+# export DOCKER_BUILDKIT=0
+# export BUILDKIT_STEP_LOG_MAX_SIZE=50000000
+# export BUILDKIT_STEP_LOG_MAX_SIZE=1073741824
+export BUILDKIT_STEP_LOG_MAX_SIZE=20971520
+# export BUILDKIT_STEP_LOG_MAX_SPEED=-1
+export BUILDKIT_STEP_LOG_MAX_SPEED=1048576
+
+# echo -e "${green} Installing roles version ${NC}"
+# ${ANSIBLE_GALAXY_CMD} install -r "${WORKING_DIR}/../requirements.yml" -p ./roles/ --ignore-errors
 
 echo -e "${green} Building docker image ${NC}"
-echo -e "${magenta} time docker build ${DOCKER_BUILD_ARGS} -f ${WORKING_DIR}/${DOCKER_FILE} -t \"$DOCKER_ORGANISATION/$DOCKER_NAME\" -t \"${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG}\" ${WORKING_DIR}/../ ${NC}"
-time docker build ${DOCKER_BUILD_ARGS} -f ${WORKING_DIR}/${DOCKER_FILE} -t "${DOCKER_ORGANISATION}/${DOCKER_NAME}" -t "${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG}" ${WORKING_DIR}/../ | tee docker.log
+echo -e "${magenta} time docker build ${DOCKER_BUILD_ARGS} -f ${WORKING_DIR}/../${DOCKER_FILE} --tag \"$DOCKER_ORGANISATION/${DOCKER_NAME}\" --tag \"${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG}\" \"${WORKING_DIR}/..\" ${NC}"
+time docker build ${DOCKER_BUILD_ARGS} -f "${WORKING_DIR}/../${DOCKER_FILE}" --tag "${DOCKER_ORGANISATION}/${DOCKER_NAME}" --tag "${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG}" "${WORKING_DIR}/../" | tee docker.log
 RC=$?
 if [ ${RC} -ne 0 ]; then
   echo ""
@@ -56,7 +58,7 @@ else
   echo -e "${green} The build completed successfully. ${NC}"
   echo -e "${magenta} Running docker history to docker history ${NC}"
   echo -e "    docker history --no-trunc ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest > docker-history.log"
-  docker history --no-trunc ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest > docker-history.log
+  docker history --no-trunc ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest >docker-history.log
   echo -e "${magenta} Running dive ${NC}"
   echo -e "    dive ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest"
   CI=true dive "${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest" || true | tee -a docker-dive.log
@@ -73,16 +75,16 @@ echo -e "${green} This image is a trusted docker hub Image. ${happy_smiley} ${NC
 echo -e "See https://hub.docker.com/r/nabla/ansible-jenkins-slave-docker/"
 echo -e ""
 echo -e "To push it"
-echo -e "    docker login ${DOCKER_REGISTRY} --username ${DOCKER_USERNAME} --password password"
-echo -e "    docker tag ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG}"
-#echo -e "    docker tag ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest"
-#echo -e "    docker tag ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${DOCKER_REGISTRY_HUB}/${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest"
-echo -e "    docker push ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}"
+echo -e "    docker login ${DOCKER_REGISTRY} --username ${DOCKER_USERNAME} --password password ${NC}"
+echo -e "    docker tag ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG} ${NC}"
+#echo -e "    docker tag ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${NC}"
+#echo -e "    docker tag ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${DOCKER_REGISTRY_HUB}/${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${NC}"
+echo -e "    docker push ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME} ${NC}"
 
-echo -e "    docker manifest inspect  ${DOCKER_REGISTRY_ACR}${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest"
+echo -e "    docker manifest inspect  ${DOCKER_REGISTRY_ACR}${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest ${NC}"
 echo -e ""
 echo -e "To pull it"
-echo -e "    docker pull ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG}"
+echo -e "    docker pull ${DOCKER_REGISTRY}${DOCKER_ORGANISATION}/${DOCKER_NAME}:${DOCKER_TAG} ${NC}"
 echo -e ""
 echo -e "To use this docker:"
 echo -e "    docker run -d -P ${DOCKER_ORGANISATION}/${DOCKER_NAME}"
@@ -95,8 +97,8 @@ export GROUP=${GROUP:-docker}
 export DOCKER_UID=${DOCKER_UID:-1004}
 export DOCKER_GID=${DOCKER_GID:-999}
 # shellcheck disable=SC2059
-printf "\033[1;32mFROM UID:GID: ${DOCKER_UID}:${DOCKER_GID}- JENKINS_USER_HOME: ${JENKINS_USER_HOME} \033[0m\n" && \
-printf "\033[1;32mWITH $USER\ngroup: $GROUP \033[0m\n"
+printf "\033[1;32mFROM UID:GID: ${DOCKER_UID}:${DOCKER_GID}- JENKINS_USER_HOME: ${JENKINS_USER_HOME} \033[0m\n" &&
+  printf "\033[1;32mWITH ${USER}\ngroup: ${GROUP} \033[0m\n"
 
 echo -e "${green} User is. ${happy_smiley} : ${NC}"
 id "${USER}"
@@ -104,12 +106,23 @@ echo -e "${magenta} Add docker group to above user. ${happy_smiley} ${NC}"
 echo -e "${magenta} sudo usermod -a -G docker ${USER} ${NC}"
 
 echo -e "To run in interactive mode for debug:"
-echo -e "    docker run --init -it -u ${DOCKER_UID}:${DOCKER_GID} --userns=host -v ${JENKINS_USER_HOME}:/home/jenkins -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /bin/bash ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest"
+echo -e "    docker run --init -it -u ${DOCKER_UID}:${DOCKER_GID} --userns=host -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /bin/bash ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest"
+echo -e ""
+echo -e "     -v ${JENKINS_USER_HOME}:/home/jenkins"
+echo -e ""
 echo -e "    docker run --init -it -d -u ${DOCKER_UID}:${DOCKER_GID} --userns=host --name sandbox ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest cat"
 echo -e "    Note: --init is necessary for correct subprocesses handling (zombie reaping)"
-echo -e "    docker run --init ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest -url http://localhost:8686/ -workDir=/home/jenkins/agent <secret> <agent name>"
-echo -e "    docker exec -it sandbox /bin/bash"
-echo -e "    docker exec -u 0 -it sandbox env TERM=xterm-256color bash -l"
+
+export JENKINS_URL=${JENKINS_URL:="http://albandrieu/jenkins"}
+#export JENKINS_CRUMB=$(curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+echo -e "JENKINS_CRUMB : ${JENKINS_CRUMB} ${NC}"
+export JENKINS_AGENT_NAME=${JENKINS_AGENT_NAME:="docker-test"}
+#export JENKINS_SECRET=$(curl -L -s -u admin:password -H "Jenkins-Crumb:${JENKINS_CRUMB}" -X GET ${JENKINS_URL}/computer/docker-test/slave-agent.jnlp | sed "s/.*<application-desc main-class=\"hudson.remoting.jnlp.Main\"><argument>\([a-z0-9]*\).*/\1/")
+echo -e "JENKINS_SECRET : ${JENKINS_SECRET} ${NC}"
+
+echo -e "    docker run --init ${DOCKER_ORGANISATION}/${DOCKER_NAME}:latest /usr/local/bin/jenkins-agent -url ${JENKINS_URL} -workDir=/home/jenkins/agent ${JENKINS_SECRET} ${JENKINS_AGENT_NAME} ${NC}"
+echo -e "    docker exec -it sandbox /bin/bash ${NC}"
+echo -e "    docker exec -u 0 -it sandbox env TERM=xterm-256color bash -l ${NC}"
 echo -e ""
 
 echo -e "${magenta} Run CST test ${NC}"
